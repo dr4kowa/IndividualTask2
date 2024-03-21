eca25ea8337fb18d0f69449bebe3a597ad73408d
dr4kowa <artemcuranov526@gmail.com>
2024-03-21 23:39:15 +0300

    Change №2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException {
         System.out.println("Практическое задание № 1.8, Индивидуальное задание № 2 \n" +
                 "Выполнил: Цуранов Артем Юрьевич, РИБО-02-22, вариант № 2 \n" +
-                "------------------------------------------------------------------");
+                "----------------------------------------------------------------------");
         HashMap<String, Telephone> telephonesMap = new HashMap<>();
         telephonesMap.put("X35235ZMD", new Telephone("Panasonic", "X35235ZMD", "white", false));
         telephonesMap.put("XYZ123456789", new Telephone("Samsung S10", "XYZ123456789", "black", true));
@@ -26,40 +26,37 @@ public class Main {
             System.out.println(index + ". " + Model + ", " + SerialNum + ", " + Color + ", " + Mobile);
         }
 
-        int flag1 = 0;
         Scanner scan = new Scanner(System.in);
+        int flag1 = 0;
         while (flag1 != 4) {
             System.out.println("Введите: 1 - для добавления телефона, 2 - для удаления телефона по серийному номеру, 3 - для удаления всего списка, 4 - для выхода:");
-            String Line = scan.nextLine();
 
-            // Поиск ошибок
-            // TDO Add hasNextInt
-            try {
-                flag1 = Integer.parseInt(Line);
-            } catch (NumberFormatException e) {
+// Поиск ошибок
+            if (scan.hasNextInt()) {
+                flag1 = scan.nextInt();
+                scan.nextLine();
+            } else {
                 System.out.println("Ошибка ввода, попробуйте снова");
-                continue;
             }
             if (flag1 < 1 || flag1 > 4) {
                 System.out.println("Ошибка ввода, попробуйте снова");
                 continue;
             }
 
-            // TDO Сделать проверку на легальность цвета
-
-            // Добавление телефона в список
+            // TODO Сделать проверку на легальность цвета
+// Добавление телефона в список
             if (flag1 == 1) {
                 String phoneModel = "";
-                while (phoneModel.isEmpty()) {
+                while (phoneModel.isBlank()) {
                     System.out.println("Введите модель телефона:");
                     phoneModel = scan.nextLine();
                 }
                 String serialNum = "";
                 boolean isNewSerialNum = false;
-                while (serialNum.isEmpty() && !isNewSerialNum) {
+                while (serialNum.isBlank() && !isNewSerialNum) {
                     System.out.println("Введите серийный номер телефона:");
                     serialNum = scan.nextLine();
-                    if (!serialNum.isEmpty()) {
+                    if (!serialNum.isBlank()) {
                         isNewSerialNum = true;
                         for (Map.Entry<String, Telephone> entr : telephonesMap.entrySet()) {
                             String SerNum = entr.getValue().getSerialNumber();
@@ -72,9 +69,17 @@ public class Main {
                     }
                 }
                 serialNum = serialNum.toUpperCase();
-                System.out.println("Введите цвет телефона:");
-                String TeleColor = scan.nextLine().toLowerCase();
-                Color color = Color.getColor(TeleColor);
+                String TeleColor;
+                while (true) {
+                    System.out.println("Введите цвет телефона:");
+                    TeleColor = scan.nextLine().toLowerCase();
+                    try {
+                        java.lang.reflect.Field field = Color.class.getField(TeleColor);
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Вы ввели несуществующий цвет");
+                    }
+                }
                 String Mobile = null;
                 boolean flag3 = true;
                 int Mob = 0;
@@ -82,7 +87,6 @@ public class Main {
                 while (flag3) {
                     System.out.println("Данный телефон - мобильный? 1 - да, 2 - нет:");
                     Mobile = scan.nextLine();
-                    // TDO hasNextInt() вместо try\catch
                     try {
                         Mob = Integer.parseInt(Mobile);
                         if (Mob == 1) {
@@ -101,7 +105,7 @@ public class Main {
                 telephonesMap.put(serialNum, new Telephone(phoneModel, serialNum, TeleColor, isMobilePhone));
             }
 
-            // Удаление телефона из списка по серийному номеру
+// Удаление телефона из списка по серийному номеру
             if (flag1 == 2) {
                 System.out.println("Введите серийный номер телефона, который Вы хотите удалить:");
                 String SerialNumberRemove = scan.nextLine().toUpperCase();
@@ -120,18 +124,18 @@ public class Main {
                 }
             }
 
-            // Удаление всего списка
+// Удаление всего списка
             if (flag1 == 3) {
                 telephonesMap.clear();
             }
 
-            // Выход из программы
+// Выход из программы
             if (flag1 == 4) {
                 System.out.println("Пока-пока");
                 break;
             }
 
-            // Вывод списка
+// Вывод списка
             index = 0;
             for (Map.Entry<String, Telephone> entry : telephonesMap.entrySet()) {
                 index += 1;
